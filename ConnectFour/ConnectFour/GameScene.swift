e02b210bd7a542c6081c06a30639ae72fb20d00e
Daniela Lubezki <Danielalubezki20@k12.xrds.org>
2020-04-27 17:12:37 -0700

    Title page
    tried to initialize a title page, didnt work out too well. It stays on top of my other game board

--- ConnectFour/ConnectFour/GameScene.swift
+++ ConnectFour/ConnectFour/GameScene.swift
@@ -12,25 +12,55 @@ import GameplayKit
 class GameScene: SKScene {
     
     var board: Board!
+    var gameLogo: SKLabelNode!
+    var bestScore: SKLabelNode!
+    var playButton: SKShapeNode!
 
     override func didMove(to view: SKView) {
+        initializeMenu()
         board = Board()
         createBoard()
     }
     
+    
+    private func initializeMenu() {
+        //Create game title
+        gameLogo = SKLabelNode(fontNamed: "ArialRoundedMTBold")
+        gameLogo.zPosition = 1
+        gameLogo.position = CGPoint(x: 0, y: (frame.size.height / 2) - 200)
+        gameLogo.fontSize = 60
+        gameLogo.text = "Connect Four"
+        gameLogo.fontColor = SKColor.purple
+        self.addChild(gameLogo)
+        playButton = SKShapeNode()
+        playButton.name = "play_button"
+        playButton.zPosition = 1
+        playButton.position = CGPoint(x: 0, y: (frame.size.height / -2) + 200)
+        playButton.fillColor = SKColor.cyan
+        let topCorner = CGPoint(x: -50, y: 50)
+        let bottomCorner = CGPoint(x: -50, y: -50)
+        let middle = CGPoint(x: 50, y: 0)
+        let path = CGMutablePath()
+        path.addLine(to: topCorner)
+        path.addLines(between: [topCorner, bottomCorner, middle])
+        playButton.path = path
+        self.addChild(playButton)
+    }
+ 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
-            let touchedNodes = self.nodes(at: location)
-            for touchedNode in touchedNodes {
-                for column in self.board.columns {
-                    if let dropper = column.dropper, dropper == touchedNode {
-                        board.dropChip(in: column)
-                        board.updateDisplay()
-                    }
+            let touchedNode = self.nodes(at: location)
+            for node in touchedNode {
+                if node.name == "play_button" {
+                    startGame()
                 }
             }
         }
+    }
+
+    private func startGame() {
+        createBoard()
     }
     
     override func update(_ currentTime: TimeInterval) {
